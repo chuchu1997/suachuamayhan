5a098662d9cf737c97b121c1b9ab4b46d2b6a7aa
cuong <cuong@foxit.co.jp>
2025-03-20 15:12:25 +0700

    add

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -16,23 +16,34 @@ export const Navbar = () => {
         <div className="flex items-center justify-between h-16">
           {/* Logo */}
           <div className="flex-shrink-0">
-            <Image  
-   src="/images/logo.png" 
-   width={100} 
-   height={50} 
-   alt="Logo"
-   style={{
-     maxWidth: '100%',
-     height: 'auto'
-   }}
-   // Prevent any layout shift during loading
-   priority={true}
-     />
+            <Image
+              src="/images/logo.png"
+              width={100}
+              height={50}
+              alt="Logo"
+              style={{
+                maxWidth: '100%',
+                height: 'auto'
+              }}
+              // Prevent any layout shift during loading
+              priority={true}
+            />
+          </div>
+                <div className = "block sm:hidden">
+                <ButtonPhoneCall />
+                </div>
+
+        
+
+
+          <div className = "hidden sm:flex flex-row gap-6  ">
+            <Image src = "/images/brands/1.jpg" height = {100} width={100} quality={100} alt = "honda" priority></Image>
+            <Image src = "/images/brands/2.jpg" height = {100} width={100} alt = "panapro" priority></Image>
+            <Image src = "/images/brands/3.jpg" height = {100} width={100} alt = "mikasa" priority></Image>
+            <Image src = "/images/brands/4.jpg" height = {100} width={100} alt = "seoul" quality={100}  priority></Image>
           </div>
 
-           <ButtonPhoneCall/>
 
-          
         </div>
       </div>
     </nav>
